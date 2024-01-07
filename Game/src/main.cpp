20336c3e897ff4847d96a1216cda9dc3ebd5f1cc
Nacho Martinez Garrido <ignacio.martinez@gameloft.com>
2024-01-07 17:41:14 +0100

    Added build scrip

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -3,11 +3,6 @@
 #include <Logger.h>
 #include <window/EWindow.h>
 
-static bool running = true;
-
-///////////////////////////////////////////////////////////////////////////////
-
-
 int main(int argc, char** argv)
 {
 	core::platform::EWindow window {{.width = 1280, .height = 720, .name = "2DGame"}};
---
+++ build.bat
@@ -0,0 +1,2 @@
+msbuild /t:"Game" /p:Configuration="Debug" /p:Platform="x64" /verbosity:minimal "2DGame.sln" /m
+
