c14f0059a5a6131b543602400c64074d8b81ad14
IgnacioMGarrido <nama1510@gmail.com>
2024-01-21 13:34:40 +0100

    Creating a SDL Window

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -4,11 +4,13 @@
 
 //TODO(Nacho): remove glovals
 
-uint16_t g_Width = 1280;
-uint16_t g_Heigh = 720;
+uint16_t g_width = 1280;
+uint16_t g_heigh = 720;
 SDL_Window* g_window = nullptr;
-SDL_GLContext* g_context = nullptr;
+SDL_GLContext g_context = nullptr;
+bool g_running = true;
 
+///////////////////////////////////////////////////////////////////////////////
 
 void InitProgram()
 {
@@ -18,12 +20,67 @@ void InitProgram()
 		exit(EXIT_FAILURE);
 	}
 
-	
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+	g_window =  SDL_CreateWindow("Game",
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, g_width,
+                              g_heigh, SDL_WINDOW_OPENGL);
+
+	LOG_ASSERT(g_window, "Failed to Create Window");
+
+	g_context = SDL_GL_CreateContext(g_window);
+
+	LOG_ASSERT(g_context, "Failed to create Opengl Context");
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+void Input()
+{
+	SDL_Event e;
+
+	while(SDL_PollEvent(&e) != 0)
+	{
+		 if(e.type == SDL_QUIT)
+		 {
+			 g_running = false;
+		 }
+	}
 
 }
 
+void PreRender()
+{
+
+}
+
+void Render()
+{
+
+}
+
+
+void MainLoop()
+{
+	while(g_running)
+	{
+		Input();
+		PreRender();
+		Render();
+		SDL_GL_SwapWindow(g_window);
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 void Cleanup()
 {
+	SDL_DestroyWindow(g_window);
 	SDL_Quit();
 }
 
@@ -32,6 +89,7 @@ void Cleanup()
 int main(int argc, char** argv)
 {
 	InitProgram();
+	MainLoop();
 	Cleanup();
 	return 0;
 }
