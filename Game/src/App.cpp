a929cb6e8a77b5a97611be7f1dbd502f647a8ad0
IgnacioMGarrido <nama1510@gmail.com>
2024-01-28 00:31:07 +0100

    Updated engine

--- Game/src/App.cpp
+++ Game/src/App.cpp
@@ -1,19 +1,48 @@
 #include <engine/platform/Logger.h>
 #include <engine/App.h>
+#include <engine/render/Drawer.h>
 #include <engine/EntryPoint.h>
 
 class App : public Core::App
 {
 public:
-	App(std::string i_appName = "MyApp")
+
+	struct Rect
+	{
+		float xpos;
+		float ypos;
+		float width;
+		float height;
+		uint8_t r;
+		uint8_t g;
+		uint8_t b;
+	};
+	Rect myRect
+	{
+		.xpos = 128,
+		.ypos = 0,
+		.width = 128,
+		.height = 32,
+		.r = 0,
+		.g = 255,
+		.b = 0
+	};
+
+	App(std::string i_appName = "BreakOut")
 		: Core::App(i_appName)
 	{
-
 	}
 
-	virtual void Update() override
+	virtual void Update(float i_fixedTick) override
 	{
-		LOG_TRACE("MyApp::Update()");
+		myRect.xpos += 0.01f * i_fixedTick;
+		LOG_TRACE("MyApp::Update() -> %.1f fps", i_fixedTick);
+	}
+
+	virtual void Render() override
+	{
+		m_drawer->DrawFilledRect(myRect.xpos, myRect.ypos, myRect.width, myRect.height, myRect.r, myRect.g, myRect.b);
+		LOG_TRACE("MyApp::Render");
 	}
 };
 
